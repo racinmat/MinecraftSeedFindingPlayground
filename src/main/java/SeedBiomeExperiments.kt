56520e9eea6220f201a526c086f437ba308be612
Matěj Račinský <racinsky.matej@seznam.cz>
2020-09-12 11:50:22 +0200

    examining 2x2 grid

--- src/main/java/SeedBiomeExperiments.kt
+++ src/main/java/SeedBiomeExperiments.kt
@@ -2,6 +2,7 @@ import Main.VERSION
 import Searcher.cartesianProduct
 import VerifyResults.fromCsv
 import kaptainwutax.biomeutils.Biome
+import kaptainwutax.biomeutils.layer.BiomeLayer
 import kaptainwutax.biomeutils.layer.land.BaseBiomesLayer
 import kaptainwutax.biomeutils.layer.land.ContinentLayer
 import kaptainwutax.biomeutils.source.OverworldBiomeSource
@@ -11,9 +12,7 @@ import kaptainwutax.seedutils.mc.ChunkRand
 import kaptainwutax.seedutils.mc.MCVersion
 import kaptainwutax.seedutils.mc.pos.CPos
 import kaptainwutax.seedutils.util.math.Vec3i
-import krangl.dataFrameOf
-import krangl.eq
-import krangl.not
+import krangl.*
 
 object SeedBiomeExperiments {
 
@@ -46,6 +45,8 @@ object SeedBiomeExperiments {
         print("end")
     }
 
+    fun biomeNameAtRPos(layer: BiomeLayer, x: Int, z: Int) = Biome.REGISTRY[layer.get(x, 0, z)]?.name
+
     //dark forest should be on layer 18, but sometimes it's not
     // and it gets there on some of scaling layers, it's there every time on layer 21
     @JvmStatic
@@ -69,10 +70,11 @@ object SeedBiomeExperiments {
 //        var results = fromCsv("old_multithread_seeds_2/distances_0_70.csv");
 ////        var results = fromCsv("broken_small/distances_0_10.csv");
 //        var results = fromCsv("good_seeds/distances_0_10.csv");
-        val results = fromCsv("mansions2examine/distances_0_120.csv")
+//        val results = fromCsv("mansions2examine/distances_0_120.csv")
+        val results = fromCsv("mansions2examine2/distances_0_43100.csv")
         val seeds = results.map { it.seed }
         val mansionRows = seeds.flatMap { seed ->
-            println("\nseed: $seed")
+//            println("\nseed: $seed")
             val structure = Mansion(VERSION)
             val origin = Vec3i(0, 0, 0)
             val rand = ChunkRand()
@@ -82,31 +84,38 @@ object SeedBiomeExperiments {
             val baseBlayer = source.getLayer(18)
             val secondScalelayer = source.getLayer(21)
             assert(baseBlayer is BaseBiomesLayer)
-            println("num mansions: ${spawnedMansionPositions.size}")
+//            println("num mansions: ${spawnedMansionPositions.size}")
             spawnedMansionPositions.map { mansionPos ->
-                println("mansion pos: ${mansionPos}")
+//                println("mansion pos: ${mansionPos}")
                 val bpos = mansionPos.toBlockPos()
-                val rpos = bpos.toRegionPos(baseBlayer.scale)
-                val bid = baseBlayer.get(rpos.x, 0, rpos.z)
-                val layer18name = Biome.REGISTRY[bid]?.name
-                println("layer 18 biome: $layer18name, should be ${Biome.DARK_FOREST.name}")
-                val rpos2 = bpos.toRegionPos(secondScalelayer.scale)
-                val bid2 = secondScalelayer.get(rpos2.x, 0, rpos2.z)
-                val layer21name = Biome.REGISTRY[bid2]?.name
-                println("layer 21 biome: $layer21name, should be ${Biome.DARK_FOREST.name}")
+                val rpos18 = bpos.toRegionPos(baseBlayer.scale)
+                val layer18name = biomeNameAtRPos(baseBlayer, rpos18.x, rpos18.z)
+                val layer18nameE = biomeNameAtRPos(baseBlayer, rpos18.x + 1, rpos18.z)
+                val layer18nameS = biomeNameAtRPos(baseBlayer, rpos18.x, rpos18.z + 1)
+                val layer18nameSE = biomeNameAtRPos(baseBlayer, rpos18.x + 1, rpos18.z + 1)
+//                println("layer 18 biome: $layer18name, should be ${Biome.DARK_FOREST.name}")
+                val rpos21 = bpos.toRegionPos(secondScalelayer.scale)
+                val layer21name = biomeNameAtRPos(secondScalelayer, rpos21.x, rpos21.z)
+//                println("layer 21 biome: $layer21name, should be ${Biome.DARK_FOREST.name}")
 //                assert(source.getBiome(bpos.x, 0, bpos.z) == Biome.DARK_FOREST)
 //                println("layer 18 biome: ${Biome.REGISTRY[bid]?.name}")
                 val surfaceName = source.getBiome(bpos.x, 0, bpos.z).name
-                println("source biome: $surfaceName")
+//                println("source biome: $surfaceName")
 
                 // base biome choose dark forest roll
-                var localSeed = Test.getLocalSeed(Test.getLayer(BaseBiomesLayer::class.java), seed, rpos.x, rpos.z)
+                var localSeed = Test.getLocalSeed(Test.getLayer(BaseBiomesLayer::class.java), seed, rpos18.x, rpos18.z)
                 val baseBiomeRoll1 = Math.floorMod(localSeed shr 24, 6) == 1
-                println("will roll dark forest in desert in BASEBIOMES $baseBiomeRoll1")
+                var localSeedS = Test.getLocalSeed(Test.getLayer(BaseBiomesLayer::class.java), seed, rpos18.x, rpos18.z+1)
+                val baseBiomeRoll1S = Math.floorMod(localSeedS shr 24, 6) == 1
+                var localSeedE = Test.getLocalSeed(Test.getLayer(BaseBiomesLayer::class.java), seed, rpos18.x+1, rpos18.z)
+                val baseBiomeRoll1E = Math.floorMod(localSeedE shr 24, 6) == 1
+                var localSeedSE = Test.getLocalSeed(Test.getLayer(BaseBiomesLayer::class.java), seed, rpos18.x+1, rpos18.z+1)
+                val baseBiomeRoll1SE = Math.floorMod(localSeedSE shr 24, 6) == 1
+//                println("will roll dark forest in desert in BASEBIOMES $baseBiomeRoll1")
                 // continent choose plains
-                localSeed = Test.getLocalSeed(Test.getLayer(ContinentLayer::class.java), seed, rpos.x, rpos.z)
+                localSeed = Test.getLocalSeed(Test.getLayer(ContinentLayer::class.java), seed, rpos18.x, rpos18.z)
                 val continentRoll0 = Math.floorMod(localSeed shr 24, 10) == 0
-                println("will roll plains in CONTINENT $continentRoll0")
+//                println("will roll plains in CONTINENT $continentRoll0")
                 // not enough structures in the region, this seed is not interesting, quitting
 //            if (structPositions.isEmpty() && structureInfo.isRequired) {
 //                GlobalState.incr(structureInfo.structName);
@@ -114,34 +123,33 @@ object SeedBiomeExperiments {
 //            }
 //            structures[structureInfo] = structPositions
 //                listOf(seed, bpos, layer18name, layer21name, surfaceName, baseBiomeRoll1, continentRoll0)
-                mapOf("seed" to seed, "bpos" to bpos, "layer18name" to layer18name, "layer21name" to layer21name,
-                        "surfaceName" to surfaceName, "baseBiomeRoll1" to baseBiomeRoll1,
+                mapOf("seed" to seed, "bpos" to bpos,
+                        "layer18name" to layer18name, "layer18nameE" to layer18nameE,
+                        "layer18nameS" to layer18nameS, "layer18nameSE" to layer18nameSE,
+                        "layer21name" to layer21name, "surfaceName" to surfaceName,
+                        "baseBiomeRoll1" to baseBiomeRoll1, "baseBiomeRoll1S" to baseBiomeRoll1S,
+                        "baseBiomeRoll1E" to baseBiomeRoll1E, "baseBiomeRoll1SE" to baseBiomeRoll1SE,
                         "continentRoll0" to continentRoll0)
 
             }
         }
         val df = dataFrameOf(mansionRows)
-        println(df)
-        println("layer18name wrong num: ${df["layer18name"].eq("dark_forest").not().count { it }}\n" +
-                "layer21name wrong num: ${df["layer21name"].eq("dark_forest").not().count { it }}\n" +
-                "surfaceName wrong num: ${df["surfaceName"].eq("dark_forest").not().count { it }}\n" +
-                "baseBiomeRoll1 wrong num: ${df["baseBiomeRoll1"].eq(true).not().count { it }}\n" +
-                "continentRoll0 wrong num: ${df["continentRoll0"].eq(true).not().count { it }}\n")
-//        df.summarize(
-//                "min_age" to { it["age"].min() },
-//                "max_age" to { it["age"].max() }
-//        )
-//        df.summarize(
-//                "min_age" to { it["age"].min() },
-//                "max_age" to { it["age"].max() }
-//        )
+        df.print(maxWidth = 300)
+        println(df.schema())
+        val summ = df.summarize(
+                "layer18name wrong num" to { it["layer18name"].eq("dark_forest").not().count { it } },
+                "layer18nameS wrong num" to { it["layer18nameS"].eq("dark_forest").not().count { it } },
+                "layer18nameE wrong num" to { it["layer18nameE"].eq("dark_forest").not().count { it } },
+                "layer18nameSE wrong num" to { it["layer18nameSE"].eq("dark_forest").not().count { it } },
+                "layer18name2x2 wrong num" to { df.rows.map {"dark_forest" !in listOf(it["layer18name"], it["layer18nameS"], it["layer18nameE"], it["layer18nameSE"]) }.count { it } },
+                "layer21name wrong num" to { it["layer18name"].eq("dark_forest").not().count { it } },
+                "surfaceName wrong num" to { it["surfaceName"].asStrings().count { it !in setOf("dark_forest", "dark_forest_hills") } },
+                "baseBiomeRoll1 wrong num" to { it["baseBiomeRoll1"].eq(false).count { it } },
+                "baseBiomeRoll12x2 wrong num" to { df.rows.map {!listOf(it["baseBiomeRoll1"], it["baseBiomeRoll1S"], it["baseBiomeRoll1E"], it["baseBiomeRoll1SE"]).any() }.count { it } },
+                "continentRoll0 wrong num" to { it["continentRoll0"].eq(false).count { it } }
+        )
+        summ.print(maxWidth = 300)
 
-// for sake of r and python adoptability you can also use `=` here
-//        df.summarize(
-//                "min_age" `=` { it["age"].min() },
-//                "max_age" `=` { it["age"].max() }
-//        )
-        //
-//        GlobalState.shutdown();
+        GlobalState.shutdown();
     }
 }
